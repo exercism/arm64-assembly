8a52e84c38f1c0e3e2f8a27e00369180f5e1f24f
keiravillekode <keiravillekode@gmail.com>
2024-10-19 11:39:08 +1100

    Add collatz-conjecture exercise (#67)

---
+++ exercises/practice/collatz-conjecture/.meta/example.s
@@ -0,0 +1,32 @@
+.text
+.globl steps
+
+/* extern int steps(int number); */
+steps:
+        mov     x1, #1
+        cmp     x0, x1
+        blt     .invalid
+
+        mov     x2, x0                  /* current number */
+        mov     x3, #3
+        mov     x0, #0                  /* step count */
+
+.loop:
+        cmp     x2, x1
+        beq     .return
+
+        tbz     x2, #0, .even           /* jump ahead if low bit is clear */
+
+        madd    x2, x3, x2, x1          /* 3 * number + 1 */
+        add     x0, x0, #1              /* increment step count */
+
+.even:
+        lsr     x2, x2, #1              /* number / 2 */
+        add     x0, x0, #1              /* increment step count */
+        b       .loop
+
+.invalid:
+        mov     x0, #-1
+
+.return:
+        ret
--- exercises/practice/leap/.meta/example.s
+++ exercises/practice/leap/.meta/example.s
@@ -8,10 +8,6 @@ leap_year:
         msub    x3, x2, x1, x0          /* remainder, i.e. year within century */
         tst     x3, x3                  /* check if 0 */
         csel    x0, x2, x3, eq          /* either century, or year within century */
-        mov     x4, #0
-        mov     x5, #1
         tst     x0, #3                  /* check if multiple of 4 */
-        csel    x0, x5, x4, eq
+        cset    x0, eq
         ret
-
-

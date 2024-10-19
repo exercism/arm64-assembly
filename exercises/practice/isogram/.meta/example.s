964811988b63833ff2a3cd792ed3f49aad73b4ce
keiravillekode <keiravillekode@gmail.com>
2024-10-19 21:12:29 +1100

    Add isogram exercise (#73)

--- exercises/practice/isogram/.meta/example.s
+++ exercises/practice/isogram/.meta/example.s
@@ -1,15 +1,15 @@
 .text
-.globl is_pangram
+.globl is_isogram
 
-/* extern int is_pangram(const char *phrase); */
-is_pangram:
+/* extern int is_isogram(const char *phrase); */
+is_isogram:
         mov     x1, x0
         mov     x0, #0                  /* bitset of letters */
         mov     x2, #1
 
 .loop:
         ldrb    w3, [x1], #1            /* load byte, with post-increment */
-        cbz     x3, .return
+        cbz     x3, .accept
 
         orr     x3, x3, #32             /* force lower-case */
         sub     x3, x3, #97
@@ -17,10 +17,16 @@ is_pangram:
         bhs     .loop                   /* unsigned >= */
 
         lsl     x3, x2, x3              /* 1 << (letter - 'a') */
+        tst     x0, x3
+        bne     .reject
+
         orr     x0, x0, x3
         b       .loop
 
-.return:
-        add     x0, x0, #1              /* 2**26 iff sentence contains all letters */
-        lsr     x0, x0, #26
+.accept:
+        mov     x0, #1
+        ret
+
+.reject:
+        mov     x0, #0
         ret

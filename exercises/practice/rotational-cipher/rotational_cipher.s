ce9af728a893d28f05c488e8fa3acab88cf0d57d
keiravillekode <keiravillekode@gmail.com>
2024-10-21 19:58:08 +1100

    Add rotational-cipher exercise (#81)

---
+++ exercises/practice/rotational-cipher/rotational_cipher.s
@@ -0,0 +1,5 @@
+.text
+.globl rotate
+
+rotate:
+    ret
---
+++ exercises/practice/rotational-cipher/rotational_cipher_test.c
@@ -0,0 +1,105 @@
+#include "vendor/unity.h"
+
+#define BUFFER_SIZE 80
+
+extern void rotate(char *buffer, const char *text, int shift_key);
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_rotate_a_by_0_same_output_as_input(void) {
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "a", 0);
+    TEST_ASSERT_EQUAL_STRING("a", buffer);
+}
+
+void test_rotate_a_by_1(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "a", 1);
+    TEST_ASSERT_EQUAL_STRING("b", buffer);
+}
+
+void test_rotate_a_by_26_same_output_as_input(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "a", 26);
+    TEST_ASSERT_EQUAL_STRING("a", buffer);
+}
+
+void test_rotate_m_by_13(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "m", 13);
+    TEST_ASSERT_EQUAL_STRING("z", buffer);
+}
+
+void test_rotate_n_by_13_with_wrap_around_alphabet(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "n", 13);
+    TEST_ASSERT_EQUAL_STRING("a", buffer);
+}
+
+void test_rotate_capital_letters(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "OMG", 5);
+    TEST_ASSERT_EQUAL_STRING("TRL", buffer);
+}
+
+void test_rotate_spaces(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "O M G", 5);
+    TEST_ASSERT_EQUAL_STRING("T R L", buffer);
+}
+
+void test_rotate_numbers(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "Testing 1 2 3 testing", 4);
+    TEST_ASSERT_EQUAL_STRING("Xiwxmrk 1 2 3 xiwxmrk", buffer);
+}
+
+void test_rotate_punctuation(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "Let's eat, Grandma!", 21);
+    TEST_ASSERT_EQUAL_STRING("Gzo'n zvo, Bmviyhv!", buffer);
+}
+
+void test_rotate_all_letters(void) {
+    TEST_IGNORE();
+    char buffer[BUFFER_SIZE];
+
+    rotate(buffer, "The quick brown fox jumps over the lazy dog.", 13);
+    TEST_ASSERT_EQUAL_STRING("Gur dhvpx oebja sbk whzcf bire gur ynml qbt.", buffer);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_rotate_a_by_0_same_output_as_input);
+    RUN_TEST(test_rotate_a_by_1);
+    RUN_TEST(test_rotate_a_by_26_same_output_as_input);
+    RUN_TEST(test_rotate_m_by_13);
+    RUN_TEST(test_rotate_n_by_13_with_wrap_around_alphabet);
+    RUN_TEST(test_rotate_capital_letters);
+    RUN_TEST(test_rotate_spaces);
+    RUN_TEST(test_rotate_numbers);
+    RUN_TEST(test_rotate_punctuation);
+    RUN_TEST(test_rotate_all_letters);
+    return UNITY_END();
+}

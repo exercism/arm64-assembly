c6523f9c91f7172371f6d0288627e36e17ee1864
Eric Willigers <keiravillekode@gmail.com>
2024-10-22 18:47:06 +1100

    Add largest-series-product exercise

---
+++ generators/exercises/largest_series_product.py
@@ -0,0 +1,36 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+#include <stdint.h>
+
+#define INVALID_CHARACTER -1
+#define NEGATIVE_SPAN -2
+#define INSUFFICIENT_DIGITS -3
+
+extern int64_t largest_product(int span, const char *digits);
+"""
+
+def extra_cases():
+    return [
+        {
+            "description": "large span",
+            "property": "largestProduct",
+            "input": {
+            "digits": "98765432123456789",
+            "span": 17
+            },
+            "expected": 131681894400
+        }
+    ]
+
+def gen_func_body(prop, inp, expected):
+    span = inp["span"]
+    digits = inp["digits"]
+    if expected.__class__ == dict:
+        expectations = {}
+        expectations["digits input must only contain digits"] = "INVALID_CHARACTER"
+        expectations["span must not be negative"] = "NEGATIVE_SPAN"
+        expectations["span must be smaller than string length"] = "INSUFFICIENT_DIGITS"
+        expected = expectations[expected["error"]]
+
+    return f"TEST_ASSERT_EQUAL_INT({expected}, largest_product({span}, \"{digits}\"));\n"

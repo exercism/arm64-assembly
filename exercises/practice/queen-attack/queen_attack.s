9774723ba35fc2dee46effb7e1bf22a71db80f91
Eric Willigers <keiravillekode@gmail.com>
2024-11-02 07:42:13 +1100

    Add queen-attack exercise

---
+++ exercises/practice/queen-attack/queen_attack.s
@@ -0,0 +1,9 @@
+.text
+.globl can_create
+.globl can_attack
+
+can_create:
+    ret
+
+can_attack:
+    ret
---
+++ generators/exercises/queen_attack.py
@@ -0,0 +1,28 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+// Returns 1 if queen can be created, otherwise 0.
+extern int can_create(int row, int column);
+
+// Returns 1 if queens attack, otherwise 0.
+extern int can_attack(int white_row, int white_column, int black_row, int black_column);
+"""
+
+def gen_func_body(prop, inp, expected):
+    macro = "TEST_ASSERT_TRUE"
+    if expected is False or expected.__class__ == dict:
+        macro = "TEST_ASSERT_FALSE"
+
+    def describe(queen):
+        row = queen["position"]["row"]
+        column = queen["position"]["column"]
+        return f"{row}, {column}"
+
+    if prop == 'create':
+        queen = describe(inp["queen"])
+        return f"{macro}(can_create({queen}));\n"
+
+    if prop == 'can_attack':
+        white_queen = describe(inp["white_queen"])
+        black_queen = describe(inp["black_queen"])
+        return f"{macro}(can_attack({white_queen}, {black_queen}));\n"

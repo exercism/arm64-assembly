452273139741ec61f85797791b81ba50bfee13df
keiravillekode <keiravillekode@gmail.com>
2024-11-09 12:17:05 +1100

    Add roman-numerals exercise (#108)

---
+++ exercises/practice/roman-numerals/roman_numerals.s
@@ -0,0 +1,5 @@
+.text
+.globl roman
+
+roman:
+    ret
---
+++ generators/exercises/roman_numerals.py
@@ -0,0 +1,15 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+#define BUFFER_SIZE 20
+
+extern void roman(char *buffer, unsigned number);
+"""
+
+def gen_func_body(prop, inp, expected):
+    number = inp["number"]
+    str_list = []
+    str_list.append(f'char buffer[BUFFER_SIZE];\n\n')
+    str_list.append(f'roman(buffer, {number});\n')
+    str_list.append(f'TEST_ASSERT_EQUAL_STRING("{expected}", buffer);\n')
+    return "".join(str_list)

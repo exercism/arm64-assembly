41d13a0e22d32c810e832c2bc9b62b6ac1f80ef4
keiravillekode <keiravillekode@gmail.com>
2025-01-07 18:09:47 +1100

    Add space-age exercise (#140)

---
+++ exercises/practice/space-age/space_age.s
@@ -0,0 +1,14 @@
+.equ MERCURY, 1
+.equ VENUS, 2
+.equ EARTH, 3
+.equ MARS, 4
+.equ JUPITER, 5
+.equ SATURN, 6
+.equ URANUS, 7
+.equ NEPTUNE, 8
+
+.text
+.globl age
+
+age:
+    ret
---
+++ generators/exercises/space_age.py
@@ -0,0 +1,21 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+typedef enum {
+    MERCURY = 1,
+    VENUS,
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN,
+    URANUS,
+    NEPTUNE
+} planet_t;
+
+extern double age(planet_t planet, int seconds);
+"""
+
+def gen_func_body(prop, inp, expected):
+    planet = inp["planet"].upper()
+    seconds = inp["seconds"]
+    return f"TEST_ASSERT_FLOAT_WITHIN(0.01, {expected}, {prop}({planet}, {seconds}));\n"

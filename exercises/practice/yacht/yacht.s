ef0c9df5f3a859406b6dc661f5846011a5bdddd8
keiravillekode <keiravillekode@gmail.com>
2025-05-09 14:02:54 +1000

    Add yacht (#156)

---
+++ exercises/practice/yacht/yacht.s
@@ -0,0 +1,18 @@
+.equ CHOICE, 0
+.equ ONES, 1
+.equ TWOS, 2
+.equ THREES, 3
+.equ FOURS, 4
+.equ FIVES, 5
+.equ SIXES, 6
+.equ LITTLE_STRAIGHT, 7
+.equ BIG_STRAIGHT, 8
+.equ FULL_HOUSE, 9
+.equ FOUR_OF_A_KIND, 10
+.equ YACHT, 11
+
+.text
+.globl score
+
+score:
+        ret

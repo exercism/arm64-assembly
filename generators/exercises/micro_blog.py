95eac9127e52af649bc04555178015461f2f75ee
keiravillekode <keiravillekode@gmail.com>
2025-08-17 07:53:57 +1000

    Additional micro-blog test case (#200)
    copied from @oxe-i x86-64-assembly

--- generators/exercises/micro_blog.py
+++ generators/exercises/micro_blog.py
@@ -7,6 +7,17 @@ extern void truncate(char *buffer, const char *phrase);
 """
 
 
+def extra_cases():
+    return [
+        {
+            "description": "ideograms",
+            "property": "truncate",
+            "input": {"phrase": "二兎を追う者は一兎をも得ず"},
+            "expected": "二兎を追う",
+        }
+    ]
+
+
 def gen_func_body(prop, inp, expected):
     phrase = inp["phrase"]
     str_list = []
